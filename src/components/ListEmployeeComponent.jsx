18d513450027456a56b95297708b8f1380aee414
Capelo HOUANDJA <synedcap@gmail.com>
2023-07-19 14:16:27 +0100

    implementation of navbar component,footer component, list employee component, display list employee from api

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { listEmployees } from '../services/EmployeeService';
+
+
+const ListEmployeeComponent = () => {
+
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(error => { 
+            console.error(error)
+        })
+    },[])
+
+    return (
+        <div className='container mt-4 mb-4'>
+            <h2 className='text-center'>List of Employees</h2>
+
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Employee Id</th>
+                        <th>Employee First Name</th>
+                        <th>Employee  Last Name</th>
+                        <th>Employee Email</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        
+                   employees.map(employee =>
+                    <tr key={employee.id}>
+                        <td >{employee.id}</td>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{employee.email}</td>
+                    </tr>)
+                    }
+
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default ListEmployeeComponent;
